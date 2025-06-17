9e144989736a21f34db6e4a4ccc8ca17708be4c6
OlgaBorzdyko <helga.borzdyko@gmail.com>
2025-06-17 13:19:39 +0400

    Added product page logic

--- src/catalogue/ProductPage.tsx
+++ src/catalogue/ProductPage.tsx
@@ -1,11 +1,21 @@
 import { useParams } from 'react-router-dom'
 
+import { categoriesArray } from '../categoriesArray'
+
 const ProductPage = () => {
   const { id } = useParams()
+  const allProducts = categoriesArray
+    .filter((c) => Array.isArray(c.products))
+    .flatMap((c) => c.products)
+  console.log(allProducts)
+  const numericId = parseInt(id || '', 10)
+  const product = allProducts.find((p) => p.id === numericId)
 
   return (
     <div style={{ color: 'black' }}>
-      <h1>Товар #{id}</h1>
+      <h1>{product?.itemName}</h1>
+      <img alt={product?.itemName} src={product?.img} width={300} />
+      <p>Цена: {product?.price} ₽</p>
     </div>
   )
 }
